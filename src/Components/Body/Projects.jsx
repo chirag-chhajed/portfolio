bca38ed2d475741c909bed6cbb55d537e3660718
Chirag Chhajed <chhajedchirag18@gmail.com>
2022-11-28 15:50:46 +0530

    change made

--- src/Components/Body/Projects.jsx
+++ src/Components/Body/Projects.jsx
@@ -1,12 +1,13 @@
 import data from "../data"
 import styles from "../../css/project.module.css"
 
-function ProjectCard({ img, id, github, deployed }) {
+function ProjectCard({ img, id, github, deployed,name }) {
     return (
         <div key={id} className={styles.projectcard}>
             <div className={styles.projectimage}>
                 <img src={img} alt="image" />
             </div>
+            <h2>{name}</h2>
             <div>
                 <a href={github}>GitHub</a>
                 <a href={deployed}>Deployed</a>
--- src/Components/data.js
+++ src/Components/data.js
@@ -5,30 +5,35 @@ export default [
         img: "/assets/taskimg.png",
         id:nanoid(),
         github: "https://github.com/chirag-chhajed/beginner-cohort/tree/main/beginner-cohort/Month%202/Week%203",
-        deployed: "https://pokedex-crework.netlify.app/"
+        deployed: "https://pokedex-crework.netlify.app/",
+        name: "Pokedex"
     },
     {
         img: "/assets/sneakerapp.png",
         id:nanoid(),
         github: "https://github.com/chirag-chhajed/react-ecommerce-app",
-        deployed: " https://ecommercee-app.netlify.app"
+        deployed: " https://ecommercee-app.netlify.app",
+        name: "Sneaker App"
     },
     {
         img:"/assets/mememaker.png",
         id:nanoid(),
         github: "https://github.com/chirag-chhajed/api-react",
-        deployed: "https://react-api-crework.netlify.app"
+        deployed: "https://react-api-crework.netlify.app",
+        name: "Meme Maker"
     },
     {
         img:"/assets/loginform.png",
         id:nanoid(),
         github: "https://github.com/chirag-chhajed/beginner-cohort/tree/main/beginner-cohort/Month%201/Week%204",
-        deployed: "https://task4-crework.netlify.app/"
+        deployed: "https://task4-crework.netlify.app/",
+        name: "Login Form"
     },
     {
         img:"/assets/BWUPage.png",
         id:nanoid(),
         github: "https://github.com/chirag-chhajed/beginner-cohort/tree/main/beginner-cohort/Month%201/Week%203/Crework%20Clone",
-        deployed: "https://task3-crework.netlify.app/html/bwu"
+        deployed: "https://task3-crework.netlify.app/html/bwu",
+        name: "Crework Site Clone"
     }
 ]
